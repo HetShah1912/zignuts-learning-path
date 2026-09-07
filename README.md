67e1e59634831ed6a030817f470852578cf07645
Het Shah <shahhet.1912@gmail.com>
2026-09-07 10:27:46 +0530

    Updated Readme

--- README.md
+++ README.md
@@ -21,7 +21,8 @@ A collection of my **learning path assignments, practice code, projects, notes, 
 | Day 9  | 02/09/26 | Session-3 | Bootstrap with Project                                |
 | Day 10 | 03/09/26 | Session-3 | Tailwind Project & Figma Basics, Read Seahawk Article |
 | Day 11 | 04/09/26 | Session-3 | Figma Project : From Design to Code                   |
-| Day 12 | 05/09/26 | Session-4 | Started JS Concepts                                   |
+| Day 12 | 05/09/26 | Session-4 | JS Concepts                                           |
+| Day 13 | 06/09/26 | Session-4 | JS Concepts                                           |
 
 ## 🎯 Purpose
 
---
+++ Session-4/index.js
@@ -0,0 +1,68 @@
+// Number
+let numberLength = 16;
+let numberWeight = 7.5;
+
+// BigInt
+let bigIntX = 1234567890123456789012345n;
+let bigIntY = BigInt("1234567890123456789012345");
+
+// Strings
+let stringColor = "Yellow";
+let stringLastName = "Johnson";
+
+// Boolean
+let booleanTrue = true;
+let booleanFalse = false;
+
+// Undefined
+let undefinedX;
+let undefinedY;
+
+// Null
+let nullX = null;
+let nullY = null;
+
+// Symbol
+const symbolX = Symbol();
+const symbolY = Symbol();
+
+// Object
+const personObject = {
+    firstName: "John",
+    lastName: "Doe"
+};
+
+// Array Object
+const carsArray = ["Saab", "Volvo", "BMW"];
+
+// Date Object
+const dateObject = new Date("2022-03-25");
+
+
+// Printing all variables
+console.log("Number Length:", numberLength);
+console.log("Number Weight:", numberWeight);
+
+console.log("BigInt X:", bigIntX);
+console.log("BigInt Y:", bigIntY);
+
+console.log("String Color:", stringColor);
+console.log("String Last Name:", stringLastName);
+
+console.log("Boolean True:", booleanTrue);
+console.log("Boolean False:", booleanFalse);
+
+console.log("Undefined X:", undefinedX);
+console.log("Undefined Y:", undefinedY);
+
+console.log("Null X:", nullX);
+console.log("Null Y:", nullY);
+
+console.log("Symbol X:", symbolX);
+console.log("Symbol Y:", symbolY);
+
+console.log("Person Object:", personObject);
+
+console.log("Cars Array:", carsArray);
+
+console.log("Date Object:", dateObject);
