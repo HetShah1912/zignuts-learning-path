69fb4cbc3f424a39f3a573f82397c523066105fb
Het Shah <shahhet.1912@gmail.com>
2026-09-07 12:57:34 +0530

    JS Practice Completed

--- Session-4/index.js
+++ Session-4/index.js
@@ -1,4 +1,6 @@
-// Data Type Practice
+// Data Types
+console.log("---Data types---");
+
 // Number
 let numberLength = 16;
 let numberWeight = 7.5;
@@ -29,15 +31,15 @@ const symbolY = Symbol();
 
 // Object
 const personObject = {
-    firstName: "John",
-    lastName: "Doe"
+  firstName: "John",
+  lastName: "Doe"
 };
 
 // Array Object
 const carsArray = ["Saab", "Volvo", "BMW"];
 
 // Date Object
-const dateObject = new Date("2022-03-25");
+const dateObject = new Date("2026-09-06");
 
 
 // Printing all variables
@@ -65,5 +67,167 @@ console.log("Symbol Y:", symbolY);
 console.log("Person Object:", personObject);
 
 console.log("Cars Array:", carsArray);
-
 console.log("Date Object:", dateObject);
+
+// Operators
+
+// Assignment
+console.log("---Assignment Operators---");
+let x = 10;
+let y = 20;
+x += y;
+console.log(x);
+x -= y;
+console.log(x);
+x *= y;
+console.log(x);
+x **= y;
+console.log(x);
+x /= y;
+console.log(x);
+x %= y;
+console.log(x);
+
+// Arithmatic
+console.log("---Arithmatic Operators---");
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x ** y);
+console.log(x % y);
+
+// Logical
+console.log("---Logical Operators---");
+// AND
+let logicalAndX = true;
+let logicalAndy = logicalAndX &&= 10;
+console.log(logicalAndy)
+// OR
+let logicalOrX = false;
+let logicalOrY = x ||= 10;
+console.log(logicalOrY)
+// Null Coalescing
+let nullCoalescing;
+nullCoalescing ??= 10;
+console.log(nullCoalescing);
+
+// Comparison
+console.log("---Comparison Operator---");
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+console.log(result);
+result = text1 > text2;
+console.log(result);
+result = text1 <= text2;
+console.log(result);
+result = text1 >= text2;
+console.log(result);
+result = text1 != text2;
+console.log(result);
+
+
+// Conditionals
+console.log("---Conditionals---");
+// If else
+let age = 50;
+if (age > 12) {
+  console.log("Teen");
+}
+else if (age > 18) {
+  console.log("Young Adult");
+}
+else if (age > 35) {
+  console.log("Middle Adult");
+}
+else {
+  console.log("Late Adult");
+}
+
+// Switch
+
+let day = 3;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+
+  case 2:
+    console.log("Tuesday");
+    break;
+
+  case 3:
+    console.log("Wednesday");
+    break;
+
+  case 4:
+    console.log("Thursday");
+    break;
+
+  case 5:
+    console.log("Friday");
+    break;
+
+  default:
+    console.log("Invalid day");
+}
+
+// Ternary
+let number = 7;
+let numType = number % 2 === 0 ? "Even" : "Odd";
+console.log(numType);
+
+
+// Loops
+console.log("---Loops---");
+// For
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+// While
+let i = 1;
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+// Do... While
+let j = 1;
+do {
+  console.log(j);
+  j++;
+} while (j <= 5);
+
+// For In (Key/Property based)
+let person = {
+  name: "Het",
+  age: 22,
+  city: "Ahmedabad"
+};
+
+for (let key in person) {
+  console.log(key, person[key]);
+}
+
+// For Of (Value based)
+let fruits = ["Apple", "Banana", "Mango"];
+
+for (let fruit of fruits) {
+  console.log(fruit);
+}
+
+// Break
+for (let i = 1; i <= 10; i++) {
+  if (i === 5) {
+    break;
+  }
+  console.log(i);
+}
+// Continue
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) {
+    continue;
+  }
+  console.log(i);
+}
